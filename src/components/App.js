81bc61eda1106bf2dbdc52dc89e4c26bbea75084
constantine <constantine.kiss@gmail.com>
2021-09-29 18:19:33 +0300

    add App in components

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import Profile from "./Profile/Profile";
+import Statistics from "./Statistics/Statistics";
+import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import user from "../user.json";
+import statisticalData from "../statistical-data.json";
+import friends from "../friends.json";
+import transactions from "../transactions.json";
+
+function App() {
+  return (
+    <div className="container">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
+
+export default App;
